adf07656aad4e3a4f738e27e6eaf2f8cf8f30daf
Trent Stromkins <trent@smallrobot.org>
2015-07-02 10:18:53 -0700

    start services add

--- tests/cucumber/features/step_definitions/site.js
+++ tests/cucumber/features/step_definitions/site.js
@@ -8,7 +8,6 @@
 
     var helper = this;
 
-
     this.Given(/^I am on the homepage$/, function (callback) {
       // Write code here that turns the phrase above into concrete actions
       helper.world.browser.
