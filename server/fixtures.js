dadda6c4fd36c6b42125c0b296dfe3b1fc3007ec
Trent Stromkins <trent@smallrobot.org>
2017-01-05 16:22:26 -0800

    add fixtures

---
+++ server/fixtures.js
@@ -0,0 +1,23 @@
+if (Site.find().count() === 0) {
+  Site.insert({
+  companyAddress: "100 The Pointe, 999 Canada Place",
+  companyCity: "Vancouver",
+  companyCountry: "Canada",
+  companyEmail: "test@servicebay.ca",
+  companyFax: "604-555-4321",
+  companyName: "Small Robot",
+  companyPhone: "604-555-4321",
+  companyState: "BC",
+  contactIntro: "Contact Introduction Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut dui ut nunc lacinia rhoncus at venenatis purus. Integer lorem tortor, vestibulum nec quam ac, aliquet lacinia neque.",
+  domain: "localhost:3000",
+  featuresIntro: "<p>ServiceBay has many of the key features you will need to build a presence on the Internet, which is where your potential customers are looking for you. With setup and site configuration being handled, all you need to focus on is filling out your information, and choosing a design. We take you through all the steps with clear instructions, along with tips and tricks to make your website shine.</p>",
+  homeIntro: "<h2>ServiceBay is a website tool for the smaller mechanic shops, from backyard tinker's, to specialty automotive repair.</h2> \n<p>ServiceBay helps business owners promote their services, share a bit of their history, and share their passion for the vehicles they work on. Keeping it simple, and focussing on the needs of the business, all for a great price.</p>",
+  homeSplash: "/img/home-splash.jpg",
+  siteId: "1",
+  siteTheme: "Master",
+  siteTitle: "ServiceBay - Beautifully Easy Auto Service Websites",
+  storeIntro: "Gallery Introduction Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut dui ut nunc lacinia rhoncus at venenatis purus. Integer lorem tortor, vestibulum nec quam ac, aliquet lacinia neque.",
+  lat: 49.103233199999998249,
+  lng: -121.92899090000000228
+  });
+}
